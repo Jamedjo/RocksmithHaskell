3104117f54a0c34c13fe8e65bde067d70353cd74
James EdJo <jamedjo@gmail.com>
2014-11-13 05:41:16 +0000

    Rejects Psarc header if magic number is wrong

--- RocksmithPsarc.hs
+++ RocksmithPsarc.hs
@@ -1,17 +1,53 @@
 module RocksmithPsarc where
 
 import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy.Char8 as C
 import Data.Binary.Get
+import Data.Binary
 import Data.Word
 import Data.Functor ((<$>))
+import Control.Applicative ((<*>))
 
 -- Word32 is a 32bit unsigned integer
-data PsarcHeader = PsarcHeader { magicNumber :: Word32 }
 
-readPsarcHeader :: String -> IO PsarcHeader
-readPsarcHeader path = do
-  input <- B.readFile path
-  return (runGet matchHeader input)
+data PsarcHeader = PsarcHeader
+  deriving (Show, Eq)
 
-matchHeader :: Get PsarcHeader -- Maybe PsarcHeader
-matchHeader = PsarcHeader <$> getWord32be
+data PsarcHeaderInternal = PsarcHeaderInternal { magicNumber :: Word32, header :: PsarcHeader}
+  deriving (Show, Eq)
+
+readPsarcHeader :: String -> IO (Either String PsarcHeader)
+readPsarcHeader path = matchHeader <$> B.readFile path
+
+
+validFileHeader :: PsarcHeaderInternal -> Bool
+validFileHeader PsarcHeaderInternal {magicNumber=mn} = wordToString mn == "PSAR"
+  where wordToString = C.unpack . encode
+
+rejectUnless :: (a -> Bool) -> s -> a -> Either s a
+rejectUnless condition msg a
+    | condition a = Right a
+    | otherwise = Left msg
+
+validateHeader :: Either String PsarcHeaderInternal -> Either String PsarcHeader
+validateHeader headerInternal = header <$> do
+  hi <- headerInternal
+  hi <- rejectUnless validFileHeader "Not a valid Psarc file" hi
+  --hi <- rejectUnless recognizedFileVersion "Unknown psarc version" hi
+  --hi <- rejectUnless zlibCompression "Unknown compression method" hi
+  return hi -- get main header from header internal
+
+matchHeader :: B.ByteString -> Either String PsarcHeader
+matchHeader input = validateHeader (getHeaderInternal input)
+
+getHeaderInternal :: B.ByteString -> Either String PsarcHeaderInternal
+getHeaderInternal = eitherResult . runGetOrFail getHeader
+  where
+    eitherResult :: Either (t1, t2, a)  (t3, t4, b) -> Either a b
+    eitherResult (Left (_,_,a)) = (Left a)
+    eitherResult (Right (_,_,a)) = (Right a)
+
+getHeader :: Get PsarcHeaderInternal
+getHeader = do
+  magicNumber <- getWord32be
+  return $! PsarcHeaderInternal magicNumber PsarcHeader
--- RocksmithPsarcSpec.hs
+++ RocksmithPsarcSpec.hs
@@ -1,6 +1,7 @@
 import RocksmithPsarc
 import Test.Hspec
 import System.Environment (getArgs, withArgs)
+import qualified Data.ByteString.Lazy as B
 
 main = do
   args <- getArgs
@@ -9,8 +10,17 @@ main = do
 main' (psarcPath:hspecArgs) = withArgs hspecArgs (testReadPsarc psarcPath)
 main' _  = putStrLn "This test needs the path to a psarc file as an argument"
 
+zeroBytes n = take n $ repeat 0x00
+zeroWords n = zeroBytes (4*n)
+
+isPsarcHeaderInternal :: Either String PsarcHeaderInternal -> Bool
+isPsarcHeaderInternal (Right (PsarcHeaderInternal {})) = True
+isPsarcHeaderInternal _ = False
 
 testReadPsarc psarcPath = hspec $ do
-  describe "readPsarcHeader" $ do
-    it "reads the header from a psarc file" $
-      fmap (fromIntegral . magicNumber) (readPsarcHeader psarcPath) >>= (`shouldSatisfy` (>100))
+  describe "matchHeader" $ do
+    it "extracts the psarc magic number from the file header" $
+      (getHeaderInternal $ B.pack ([0x50, 0x53, 0x41, 0x52] ++ (zeroWords 1))) `shouldSatisfy` isPsarcHeaderInternal
+    it "fails if file doesn't have a psarc header" $
+      (matchHeader $ B.pack $ zeroWords 2) `shouldBe` Left "Not a valid Psarc file"
+      -- fmap (fromIntegral . magicNumber) (readPsarcHeader psarcPath) >>= (`shouldSatisfy` (>100))
