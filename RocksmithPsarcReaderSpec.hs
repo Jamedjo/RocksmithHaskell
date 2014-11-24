d08fa552a329c3bbaaf32652e7d59f2fd93da694
Jamedjo <jamedjo@gmail.com>
2014-11-24 01:40:58 +0000

    Real tests for PsarcReader instead of just displaying the output.

--- RocksmithPsarcReaderSpec.hs
+++ RocksmithPsarcReaderSpec.hs
@@ -1,7 +1,9 @@
 import RocksmithPsarcReader
+import RocksmithPsarcEntry
 import RocksmithPsarcIndex
 import Test.Hspec
 import System.Environment (getArgs, withArgs)
+import Control.Applicative ((<*>))
 
 main = do
   args <- getArgs
@@ -11,7 +13,18 @@ main' (psarcPath:hspecArgs) = withArgs hspecArgs (testReadPsarc psarcPath)
 main' _  = putStrLn "This test needs the path to a psarc file as an argument"
 
 testReadPsarc psarcPath = hspec $ do
-  describe "readPsarc" $ do
-    it "can read a real file" $ do
-      (fmap.fmap) (entries . index) (readPsarc psarcPath) >>= (`shouldBe` Right [])
-      --readPsarc psarcPath >>= (`shouldSatisfy` isRight)
+  describe "readPsarc can read a real file" $ do
+    it "contains many PsarcEntries" $
+      length . entries . index `shouldSatisfyRightOf` (>0)
+    it "has increasing zipLengthsIndex" $
+      (map zipLengthsIndex) . entries . index `shouldSatisfyRightOf` increasing
+    it "has increasing fileOffsets" $
+      (map fileOffset) . entries . index `shouldSatisfyRightOf` increasing
+  where
+    shouldBeRightOf l r =  (fmap.fmap) l (readPsarc psarcPath) >>= (`shouldBe` Right r)
+    infix 1 `shouldBeRightOf`
+    shouldSatisfyRightOf l r = shouldBeRightOf (r . l) True
+    infix 1 `shouldSatisfyRightOf`
+
+pairwise = zip <*> tail
+increasing xs = all (uncurry (<)) $ pairwise xs
