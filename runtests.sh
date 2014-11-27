72e1f689102e6d98d6576bd4e1b240ee45557fca
Jamedjo <jamedjo@gmail.com>
2014-11-27 02:28:55 +0000

    Lightweight test runner

---
+++ runtests.sh
@@ -0,0 +1,5 @@
+runhaskell RocksmithCryptoSpec.hs
+runhaskell RocksmithPsarcEntrySpec.hs
+runhaskell RocksmithPsarcHeaderSpec.hs "$1"
+runhaskell RocksmithPsarcIndexSpec.hs
+runhaskell RocksmithPsarcReaderSpec.hs "$1"
