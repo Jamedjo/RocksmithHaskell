f77b3cee41cbf952adbea0897d107e19352eb2c5
Jamedjo <jamedjo@gmail.com>
2014-11-28 17:04:39 +0000

    Started using ErrorT to simplify Extract Example

--- RocksmithPsarcExtractExample.hs
+++ RocksmithPsarcExtractExample.hs
@@ -5,28 +5,24 @@ import qualified Data.ByteString.Lazy.Char8 as C
 import System.Environment (getArgs)
 import System.FilePath.Glob
 import Control.Monad
+import Control.Monad.Error
 import Data.List
 import System.IO (stderr, hPutStrLn)
 
-main = do
-  args <- getArgs
-  case args of
-    (path:_) -> printFirstJson path
-    otherwise -> putStrLn "This example needs the path to a psarc file.\n\
-                          \It will extract the first json from that file to stdout"
+printErrIO :: ErrorT String IO C.ByteString -> IO ()
+printErrIO et = runErrorT et >>= (either (hPutStrLn stderr) C.putStrLn)
 
-printFirstJson :: FilePath -> IO ()
-printFirstJson path = do
-  p <- readPsarc path
-  case p of 
-    (Left msg) -> hPutStrLn stderr msg
-    (Right p) -> extractFirstJson path p >>= C.putStrLn
-
-extractFirstJson :: FilePath -> Psarc -> IO B.ByteString
-extractFirstJson path p = fExtractEntry path p (firstJson jsonEntries)
+main = printErrIO $ do
+  args <- liftIO getArgs
+  path <- ErrorT $ return $ safeHead args "This example needs the path to a psarc file.\n\
+                           \It will extract the first json from that file to stdout"
+  p <- ErrorT $ readPsarc path
+  firstJson <- ErrorT $ return $ safeHead (jsonEntries p) ("No json files found in:\n" ++ (showFileNames p))
+  liftIO $ fExtractEntry path p firstJson
   where
-    firstJson (h:_) = h
-    firstJson [] = error ("No json files found in " ++ C.unpack (C.unwords (filenames p)))
-    jsonEntries = filter ((".json" `isSuffixOf`) . C.unpack . filename) (entries p)
+    jsonEntries :: Psarc -> [PsarcEntry]
+    jsonEntries p = filter ((".json" `isSuffixOf`) . C.unpack . filename) (entries p)
+    showFileNames = C.unpack . C.unlines . filenames
 
-
+safeHead (h:_) msg = Right h
+safeHead [] msg = Left msg
