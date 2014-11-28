8e63a711b815b9ef90bec71d81258fa1aea3bdd3
Jamedjo <jamedjo@gmail.com>
2014-11-28 18:07:38 +0000

    Broke Extract Example back up into functions

--- RocksmithPsarcExtractExample.hs
+++ RocksmithPsarcExtractExample.hs
@@ -13,16 +13,22 @@ printErrIO :: ErrorT String IO C.ByteString -> IO ()
 printErrIO et = runErrorT et >>= (either (hPutStrLn stderr) C.putStrLn)
 
 main = printErrIO $ do
-  args <- liftIO getArgs
-  path <- ErrorT $ return $ safeHead args "This example needs the path to a psarc file.\n\
-                           \It will extract the first json from that file to stdout"
+  path <- ErrorT getPath
   p <- ErrorT $ readPsarc path
-  firstJson <- ErrorT $ return $ safeHead (jsonEntries p) ("No json files found in:\n" ++ (showFileNames p))
+  firstJson <- ErrorT . return $ getFirstJson p
   liftIO $ fExtractEntry path p firstJson
-  where
-    jsonEntries :: Psarc -> [PsarcEntry]
-    jsonEntries p = filter ((".json" `isSuffixOf`) . C.unpack . filename) (entries p)
-    showFileNames = C.unpack . C.unlines . filenames
+
+getPath :: IO (Either String String)
+getPath = fmap firstArg getArgs 
+  where firstArg args = safeHead args "This example needs the path to a psarc file.\n\
+                                      \It will extract the first json from that file to stdout"
+
+getFirstJson :: Psarc -> Either String PsarcEntry
+getFirstJson p = safeHead (jsonEntries p) ("No json files found in:\n" ++ (showFileNames p))
+  where showFileNames = C.unpack . C.unlines . filenames
+
+jsonEntries :: Psarc -> [PsarcEntry]
+jsonEntries p = filter ((".json" `isSuffixOf`) . C.unpack . filename) (entries p)
 
 safeHead (h:_) msg = Right h
 safeHead [] msg = Left msg
